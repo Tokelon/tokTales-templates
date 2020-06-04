5753e02523eebe218b891703cc55505c3000cd48
Tokelon <tokelontales@gmail.com>
2020-06-04 23:21:54 +0200

    Upgrade all projects to tokTales 0.2.0-rc3

--- tokTales-template-default/desktop/src/main/java/com/template/def/desktop/Main.java
+++ tokTales-template-default/desktop/src/main/java/com/template/def/desktop/Main.java
@@ -1,20 +1,21 @@
 package com.template.def.desktop;
 
-import com.tokelon.toktales.core.engine.EngineException;
-import com.tokelon.toktales.desktop.engine.inject.MasterDesktopInjectConfig;
-import com.tokelon.toktales.desktop.lwjgl.DefaultDesktopEngineLauncher;
 import com.template.def.core.GameAdapter;
+import com.tokelon.toktales.core.engine.EngineException;
+import com.tokelon.toktales.core.game.IGameAdapter;
+import com.tokelon.toktales.desktop.application.TokTalesApplication;
 
-public class Main {
+public class Main extends TokTalesApplication {
+
+
+    @Override
+    public Class<? extends IGameAdapter> makeDefaultGameAdapter() {
+        return GameAdapter.class;
+    }
 
 
     public static void main(String[] args) throws EngineException {
-        DefaultDesktopEngineLauncher launcher = new DefaultDesktopEngineLauncher(new MasterDesktopInjectConfig());
-
-        // Configure the default window size and title here
-        launcher.useDefaultWindowBuilderWith(800, 600, "Application");
-
-        launcher.launch(GameAdapter.class);
+        new Main().run(args);
     }
 
 }
--- tokTales-template-desktop/application/src/main/java/com/template/desktop/Main.java
+++ tokTales-template-desktop/application/src/main/java/com/template/desktop/Main.java
@@ -1,19 +1,20 @@
 package com.template.desktop;
 
 import com.tokelon.toktales.core.engine.EngineException;
-import com.tokelon.toktales.desktop.engine.inject.MasterDesktopInjectConfig;
-import com.tokelon.toktales.desktop.lwjgl.DefaultDesktopEngineLauncher;
+import com.tokelon.toktales.core.game.IGameAdapter;
+import com.tokelon.toktales.desktop.application.TokTalesApplication;
 
-public class Main {
+public class Main extends TokTalesApplication {
+
+
+    @Override
+    public Class<? extends IGameAdapter> makeDefaultGameAdapter() {
+        return GameAdapter.class;
+    }
 
 
     public static void main(String[] args) throws EngineException {
-        DefaultDesktopEngineLauncher launcher = new DefaultDesktopEngineLauncher(new MasterDesktopInjectConfig());
-
-        // Configure the default window size and title here
-        launcher.useDefaultWindowBuilderWith(800, 600, "Application");
-
-        launcher.launch(GameAdapter.class);
+        new Main().run(args);
     }
 
 }
